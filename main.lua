867aa2e332c1e883271b48904bc8c8068458cc09
Nathan <63228754+Nattie-G@users.noreply.github.com>
2022-01-24 01:25:59 +1100

    replaced builtin font with NotoSans from google fonts

--- main.lua
+++ main.lua
@@ -1,10 +1,15 @@
 local lg = love.graphics
 local GAME_WIDTH  = CONFIG_OPTIONS.window.width
 local GAME_HEIGHT = CONFIG_OPTIONS.window.height
-local window_width, window_height = GAME_WIDTH, GAME_HEIGHT
-local canvas = lg.newCanvas(GAME_WIDTH,GAME_HEIGHT)
+
+-- init'd in love.load
+local window_width, window_height
+local interface = {}
 
 function love.load()
+  window_width, window_height = GAME_WIDTH, GAME_HEIGHT
+  canvas = lg.newCanvas(GAME_WIDTH,GAME_HEIGHT)
+  interface.menuFont = love.graphics.newFont('fonts/NotoSans-Bold.ttf', 40)
 end
 
 function love.update()
@@ -15,9 +20,10 @@ function love.draw()
   lg.setColor(0.15, 0.20, 0.85)
   lg.rectangle('fill', 0, 0, 1600, 900)
   lg.setColor(0.90, 1.00, 0.90)
-  lg.print('Welcome to the warmup weekend!', 400, 400, 0, 4)
+  lg.setFont(interface.menuFont)
+  lg.print('Welcome to the warmup weekend!', 450, 400, 0, 1)
   local fps = love.timer.getFPS()
-  lg.print('FPS: ' .. fps, 50, 40, 0, 3)
+  lg.print('FPS: ' .. fps, 50, 40, 0, 0.75)
   lg.setCanvas()
   local stretchX = window_width / GAME_WIDTH
   local stretchY = window_height / GAME_HEIGHT
